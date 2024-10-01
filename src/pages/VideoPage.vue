5b6923b7a0183ed41a8ecfb3ae9173b42cf7cc75
tosirap <parisotpaul@live.fr>
2024-10-02 00:05:19 +0200

    video part pagination

--- src/pages/VideoPage.vue
+++ src/pages/VideoPage.vue
@@ -1,30 +1,91 @@
 <template>
   <div>
     <h1>Video Page</h1>
+    <div class="pagination">
+      <Button
+        label="Précédent"
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        icon="pi pi-chevron-left"
+        class="p-button-secondary"
+      />
+      <span class="pageNumber">Page {{ currentPage }} sur {{ totalPages }}</span>
+      <Button
+        label="Suivant"
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        icon="pi pi-chevron-right"
+        class="p-button-secondary"
+      />
+    </div>
+
     <div class="video-list">
       <VideoItem
-        v-for="(video, index) in videos"
+        v-for="(video, index) in paginatedVideos"
         :key="index"
         :title="video.title"
         :url="video.url"
+      />
+    </div>
+
+    <div class="pagination">
+      <Button
+        label="Précédent"
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        icon="pi pi-chevron-left"
+        class="p-button-secondary"
+      />
+      <span class="pageNumber">Page {{ currentPage }} sur {{ totalPages }}</span>
+      <Button
+        label="Suivant"
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        icon="pi pi-chevron-right"
+        class="p-button-secondary"
       />
     </div>
   </div>
 </template>
 
 <script>
-import VideoItem from '../components/video/VideoItem.vue';  // Assure-toi d'importer le bon fichier
+import VideoItem from '../components/video/VideoItem.vue'; // Assure-toi d'importer le bon fichier
 import { videoLinks } from '../components/video/videoLinks.ts'; // Importe les liens vidéo
+import Button from 'primevue/button'; // Importation de PrimeVue Button
 
 export default {
   name: 'VideoList',
   components: {
-    VideoItem
+    VideoItem,
+    Button // Enregistre le composant Button
   },
   data() {
     return {
-      videos: videoLinks
+      videos: videoLinks,
+      currentPage: 1,
+      itemsPerPage: 9 // Nombre de vidéos à afficher par page
     };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.videos.length / this.itemsPerPage);
+    },
+    paginatedVideos() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      return this.videos.slice(start, start + this.itemsPerPage);
+    }
+  },
+  methods: {
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    }
   }
 };
 </script>
@@ -34,5 +95,15 @@ export default {
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px;
+}
+.pageNumber{
+  padding: 20px;
 }
 </style>
